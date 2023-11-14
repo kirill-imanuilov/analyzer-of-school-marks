5eb2927458ade546a813e87acc4e58749bb235cd
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-11-14 18:05:30 +0300

    feat: add a file to insert data to the db

---
+++ insert_data.py
@@ -0,0 +1,30 @@
+from cursor import cursor, connection
+import datetime
+
+
+with open('./subjects.txt', 'r') as file:
+    subjects = file.readlines()
+    # Delete '\n'
+    for i in range(len(subjects)):
+        subjects[i] = subjects[i][:-1]
+
+input_date_message:str = 'Date: '
+input_quarter_message:str = 'Quarter: '
+
+date:str = input(input_date_message)
+quarter:str = input(input_quarter_message)
+
+if date == '':
+    date = str(datetime.date.today())
+
+with connection:
+    for elem in subjects:
+        cursor.execute(f"""
+                       INSERT INTO data (subject, mark, date, quarter) VALUES (
+                       '{elem}',
+                       '{int(input(elem + ": "))}',
+                       '{date}',
+                       '{quarter}'
+                       )
+                       """)
+
